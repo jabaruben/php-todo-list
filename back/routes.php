4a775d93b5c2a3c22a0f55066cb6fdd5c2424b6c
Ruben Carracedo <jabaruben@gmail.com>
2021-11-12 23:40:58 +0100

    Adjust routes and add new ones

--- back/routes.php
+++ back/routes.php
@@ -9,25 +9,12 @@ return [
     "routes" => [
         "private" => [
             // HTTP_METHOD, ROUTE, TARGET or CONTROLLER:METHOD, NAME, PATHTOFOLDER, CALLBACK_FUNCTION(Request, Response, $bootstrapConfig, Bootstrap)
-            ['GET', '/userspriv', 'Users:list', 'ListUsersPriv', 'users', function (ServerRequest $req, Response $res) {
-                echo "Attributes: " . print_r($req->getAttributes());
-                echo "- TEST INSIDE FUNCTION ON ROUTER MAP" . NL;
-                echo "- IS A PRIVATE SECCTION" . NL;
-                return $res;
-            }],
         ],
         "public" => [
             // HTTP_METHOD, ROUTE, TARGET or CONTROLLER:METHOD, NAME, PATHTOFOLDER, CALLBACK_FUNCTION(Request, Response, $bootstrapConfig, Bootstrap)
-            ['GET', '/users', 'Users:list', 'ListUsers', 'users', function (ServerRequest $req, Response $res) {
-                echo "Attributes: " . print_r($req->getAttributes());
-                echo "- TEST INSIDE FUNCTION ON ROUTER MAP" . NL;
-                echo "- IS A PRUBLIC SECCTION" . NL;
-                return $res;
-            }],
-            ['GET', '/users/[i:id]', 'Users:list', 'ListUsersWithId', 'users'],
-            ['GET', '/users/listnodes/[i:id]', 'Users:listNodes', 'ListUserNodes', 'users'],
-            ['GET', '/users/login/[*:user]/[a:password]', 'Users:login', 'UserLogin', 'users'],
-            ['GET', '/nodes/list/[i:id]', 'Nodes:list', 'ListNodesFromUserId', 'nodes'],
+            ['GET', '/todos', 'ToDos:list', 'ListToDos', 'ToDos'],
+            ['POST', '/todos', 'ToDos:add', 'AddToDos', 'ToDos'],
+            ['DELETE', '/todos/[i:id]', 'ToDos:delete', 'DeleteToDos', 'ToDos'],
         ],
     ],
     "errors" => [
